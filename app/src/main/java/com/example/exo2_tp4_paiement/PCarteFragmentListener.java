75b01facc2138ca46df14805cd2df20bf26cee86
BACHIR <gueyebachir98@gmail.com>
2021-04-05 10:49:13 +1300

    clique sur le bouton ok

---
+++ app/src/main/java/com/example/exo2_tp4_paiement/PCarteFragmentListener.java
@@ -0,0 +1,6 @@
+package com.example.exo2_tp4_paiement;
+
+public interface PCarteFragmentListener
+{
+    void onOkClickCarteDialog(String inputText);
+}
---
+++ app/src/main/java/com/example/exo2_tp4_paiement/PayPalFragmentListener.java
@@ -0,0 +1,6 @@
+package com.example.exo2_tp4_paiement;
+
+public interface PayPalFragmentListener
+{
+    void onOkClickPAypalDialog(String inputText);
+}
